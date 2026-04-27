2d5679b69c7e51655e0c3d48f21e0dd11d8fe042
Lucas Terriel <ls.terriel@gmail.com>
2026-04-27 17:10:43 +0200

    update contactPage

--- src/components/info/InfoContactPage.vue
+++ src/components/info/InfoContactPage.vue
@@ -2,7 +2,7 @@
   <div class="main-section">
     <p class="paragraph">Toute information complémentaire concernant l'un des imprimeurs lithographes répertoriés sera
       la bienvenue.
-      N'hésitez pas à nous contacter : <a  href="mailto:elisabeth.parinet@chartes.psl.eu?subject=Demande d'informations sur l'application Dictionnaire des imprimeurs lithographes du XIXe (ENC-PSL)">elisabeth.parinet@chartes.psl.eu</a> ; <a href="mailto:corinne.bouquin@bnf.fr?subject=Demande d'informations sur l'application Dictionnaire des imprimeurs lithographes du XIXe (ENC-PSL)">corinne.bouquin@bnf.fr</a></p>
+      N'hésitez pas à nous contacter : <a  href="mailto:elisabeth.parinet@chartes.psl.eu?subject=Demande d'informations sur l'application Dictionnaire des imprimeurs lithographes du XIXe (ENC-PSL)">Elisabeth Parinet</a> ; <a href="mailto:co.bouquin@gmail.com?subject=Demande d'informations sur l'application Dictionnaire des imprimeurs lithographes du XIXe (ENC-PSL)">Corinne Bouquin</a></p>
 
     <p class="paragraph">Pour les questions portant sur l'application, n'hésitez pas à contacter la Mission projets
       numériques de l'École nationale des chartes - PSL via ce <a
